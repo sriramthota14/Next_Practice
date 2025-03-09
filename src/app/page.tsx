e1783ef0933a0ae65d8440a877f1ad4ba39924c2
sri1718 <sriram.thota14@gmail.com>
2025-03-09 01:03:54 -0500

    added development branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Welcome To Next.JS practice Session...</li>
+          <li>Welcome To Next.JS practice Session in Development Branch...</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
